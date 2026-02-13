5969af04e951c0282c50721269b50b7b42432f6a
dev-mcknz <dev.mcknz@gmail.com>
2026-02-13 14:54:23 -0500

    add w5

--- Week05/Lab05.py
+++ Week05/Lab05.py
@@ -30,12 +30,12 @@ def fib(n):
     """
     # TODO: Base case 1 - If n equals 0, return 0
 
-
-    # TODO: Base case 2 - If n equals 1, return 1
-
-
-    # TODO: Recursive case - Return fib(n-1) + fib(n-2)
-    pass  # Remove this line when you add your code
+    def fib(n):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        return fib(n - 1) + fib(n - 2)
 
 
 # Test cases for Fibonacci
@@ -79,16 +79,25 @@ def fizz_buzz(n):
 
     # TODO: Loop from 1 to n (inclusive)
     # Hint: Use range(1, n + 1)
+    for i in range(1, n + 1):
 
         # TODO: Check if divisible by BOTH 3 and 5 FIRST
         # Hint: if i % 3 == 0 and i % 5 == 0
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
 
         # TODO: Then check if divisible by 3 only
+        elif i % 3 == 0:
+            result.append("Fizz")
 
         # TODO: Then check if divisible by 5 only
+        elif i % 5 == 0:
+            result.append("Buzz")
 
         # TODO: Otherwise, append the number as a string
         # Hint: result.append(str(i))
+        else:
+            result.append(str(i))
 
     return result
 
@@ -124,18 +133,23 @@ print("Question 3: Binary Search (#704)")
 print("=" * 50)
 
 
-# Part A: Iterative Solution
+# Part A: Iterative solution
+
 def binary_search_iterative(nums, target):
-    """
-    Search for target in sorted array using iteration.
+    left = 0
+    right = len(nums) - 1
 
-    Parameters:
-        nums (list): Sorted list of integers
-        target (int): Value to find
+    while left <= right:
+        mid = (left + right) // 2
 
-    Returns:
-        int: Index of target, or -1 if not found
-    """
+        if nums[mid] == target:
+            return mid
+        elif target < nums[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return -1
     # TODO: Initialize left pointer to 0
     left = 0
 
@@ -155,36 +169,21 @@ def binary_search_iterative(nums, target):
 
 # Part B: Recursive Solution
 def binary_search_recursive(nums, target, left, right):
-    """
-    Search for target in sorted array using recursion.
+    # Part B: Recursive solution
 
-    Parameters:
-        nums (list): Sorted list of integers
-        target (int): Value to find
-        left (int): Left boundary index
-        right (int): Right boundary index
+    def binary_search_recursive(nums, target, left, right):
+        if left > right:
+            return -1
 
-    Returns:
-        int: Index of target, or -1 if not found
-    """
-    # TODO: Base case - If left > right, return -1 (target not found)
+        mid = (left + right) // 2
 
+        if nums[mid] == target:
+            return mid
 
-    # TODO: Calculate mid = (left + right) // 2
+        if target < nums[mid]:
+            return binary_search_recursive(nums, target, left, mid - 1)
 
-
-    # TODO: If nums[mid] == target, return mid
-
-
-    # TODO: If target < nums[mid], recurse on left half
-    # Hint: return binary_search_recursive(nums, target, left, mid - 1)
-
-
-    # TODO: If target > nums[mid], recurse on right half
-    # Hint: return binary_search_recursive(nums, target, mid + 1, right)
-
-    pass  # Remove this line when you add your code
-
+        return binary_search_recursive(nums, target, mid + 1, right)
 
 # Wrapper function for recursive solution
 def search_recursive(nums, target):
